0bc1ee4fd7a1bf0145d32e9fcf45d99f55614980
technoshaman <fitosegrera@gmail.com>
2024-12-19 12:26:23 -0500

    cleaned

--- apps/admin/src/app/api/get-users-list/route.ts
+++
@@ -1,9 +0,0 @@
-import { initAdmin } from "../../../lib/firebase/admin";
-import admin from "firebase-admin";
-
-export async function GET() {
-  await initAdmin();
-  const users = await admin.auth().listUsers();
-  console.log(users);
-  return new Response("Hello, Next.js!");
-}
