f0b66f0cfbee660321b73efea1cb83df9b7a6fb3
Fito Segrerav <fitosegrerav@gmail.com>
2025-01-29 21:08:18 -0500

    contact page translations implemented

--- apps/web/src/components/pages/contact-page.tsx
+++ apps/web/src/components/pages/contact-page.tsx
@@ -1,16 +1,16 @@
 import { ContactForm } from "@/components/forms/contact-form";
+import { useTranslationHandler } from "../../hooks/useTranslationHandler";
 
 export interface ContactPageProps {}
 
 export const ContactPage = ({}: ContactPageProps) => {
+  const { t } = useTranslationHandler();
+
   return (
     <div className="flex w-full flex-col items-center justify-center gap-8 px-8 py-8">
       <div className="flex w-full flex-col items-center justify-center gap-4">
-        <h2 className="text-3xl font-bold">Contact us</h2>
-        <p className="text-center">
-          Discuss your requirements, learn about pricing, or request a
-          demonstration.
-        </p>
+        <h2 className="text-3xl font-bold">{t("contactPage.headline")}</h2>
+        <p className="text-center">{t("contactPage.subHeadline")}</p>
       </div>
       <div className="flex w-full items-center justify-center gap-8">
         <ContactForm />
