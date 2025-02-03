530bd163c631e203506932799cbada059ba324d6
Fito Segrerav <fitosegrerav@gmail.com>
2025-02-03 08:23:28 -0500

    settings page implemented

--- apps/web/src/components/pages/general-settings-page.tsx
+++ apps/web/src/components/pages/general-settings-page.tsx
@@ -1,13 +1,16 @@
 import { PageHeader } from "@/components/layouts/page-header";
 import { Separator } from "@repo/ui/components/ui/separator";
 import { AutoSave } from "../forms/auto-save";
+import { useTranslationHandler } from "@/hooks/use-translation-handler";
 
 export const GeneralSettingsPage = () => {
+  const { t } = useTranslationHandler();
+
   return (
     <div className="flex w-full flex-col gap-6">
       <PageHeader
-        title="General Settings"
-        subtitle="Manage all general settings."
+        title={t("generalSettings.headline")}
+        subtitle={t("generalSettings.subHeadline")}
       />
       <Separator className="w-full" />
       <AutoSave />
