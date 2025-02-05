61530a0070556ba1f0042a22df17646aaccb6e03
Fito Segrerav <fitosegrerav@gmail.com>
2025-02-04 20:58:25 -0500

    auth pages implemented into the cms

---
+++ apps/cms/schemaTypes/authPages/passwordResetSent.ts
@@ -0,0 +1,46 @@
+import {defineField, defineType} from 'sanity'
+
+export const passwordResetSent = defineType({
+  name: 'passwordResetSent',
+  title: 'Password Reset Sent',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'message',
+      title: 'Message',
+      type: 'string',
+    }),
+    defineField({
+      name: 'resentInMessage',
+      title: 'Resend in message',
+      type: 'object',
+      fields: [
+        defineField({
+          name: 'message',
+          title: 'Message',
+          type: 'string',
+        }),
+        defineField({
+          name: 'units',
+          title: 'Units',
+          type: 'string',
+        }),
+      ],
+    }),
+    defineField({
+      name: 'resendButtonLable',
+      title: 'Resend Button Label',
+      type: 'string',
+    }),
+    defineField({
+      name: 'backButtonLable',
+      title: 'Back Button Label',
+      type: 'string',
+    }),
+  ],
+})
