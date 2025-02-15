f9148f7f38ac40f59ea353692465de427aa3bbff
Fito Segrerav <fitosegrerav@gmail.com>
2025-02-15 11:27:43 -0500

    logic to handle new way to set wine qr codes, we also manage old qr code generated that points to dynamic path

---
+++ apps/web/src/components/pages/dynamic-wine-details-page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useDynamicDomainHandler } from "~/src/hooks/qr-code-domain/use-dynamic-domain-handler";
+
+export interface WineDetailsPageProps {
+  wineId: string;
+}
+
+export const DynamicWineDetailsPage = ({ wineId }: WineDetailsPageProps) => {
+  useDynamicDomainHandler(wineId);
+
+  return <h1>Loading...</h1>;
+};
--- apps/web/src/context/providers.tsx
+++ apps/web/src/context/providers.tsx
@@ -7,12 +7,9 @@ import { WineryProvider } from "./winery";
 import { ExplorerProvider } from "./explorer";
 import { SystemVariablesProvider } from "./system-variables";
 import { CmsProvider } from "./cms";
-import { urlDomailHandler } from "../hooks/url-domain-handler";
 import { NEXT_PUBLIC_GOOGLE_MAPS_API_KEY } from "../utils/envConstants";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
-  urlDomailHandler();
-
   return (
     <>
       <Toaster />
