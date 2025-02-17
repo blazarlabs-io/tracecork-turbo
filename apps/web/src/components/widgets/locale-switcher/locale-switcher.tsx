80bed38cd7bb520729794fac9cad0c27496214b2
Fito Segrerav <fitosegrerav@gmail.com>
2025-02-16 21:14:37 -0500

    logic to set all languages in wine details

--- apps/web/src/components/widgets/locale-switcher/locale-switcher.tsx
+++ apps/web/src/components/widgets/locale-switcher/locale-switcher.tsx
@@ -1,19 +1,37 @@
 "use client";
 
 import { LocaleSwitcherSelect } from "./locale-switcher-select";
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useTranslationHandler } from "@/hooks/use-translation-handler";
+import { usePathname } from "next/navigation";
 
 export const LocaleSwitcher = () => {
   const { locale, msg } = useTranslationHandler();
+  const pathname = usePathname();
+  const [isFullLang, setIsFullLang] = useState(false);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setIsFullLang(pathname.includes("/explore/wine"));
+    }, 300);
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [pathname]);
 
   const items = useMemo(() => {
     if (!msg["language"] || typeof msg["language"] === "string") return [];
     const commongLang = msg["language"]["common-lang"];
+    if (isFullLang) {
+      const fullLang = msg["language"]["all-lang"];
+      if (!fullLang || typeof fullLang === "string") return [];
+      if (!Array.isArray(fullLang)) return [];
+      return fullLang.map((v) => ({ value: v, label: v }));
+    }
     if (!commongLang || typeof commongLang === "string") return [];
     if (!Array.isArray(commongLang)) return [];
     return commongLang.map((v) => ({ value: v, label: v }));
-  }, [msg["language"]]);
+  }, [msg["language"], isFullLang]);
 
   const label = useMemo(() => {
     if (!msg["language"] || typeof msg["language"] === "string")
